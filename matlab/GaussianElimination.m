5d0d99bbc64dedd94225bf5456d0931014e05155
Hughfitz24 <hughfitz24@gmail.com>
2025-04-01 23:14:01 +0100

    ADD: Diode Curve Fitting and gaussian elimination from MS1

---
+++ matlab/GaussianElimination.m
@@ -0,0 +1,91 @@
+%% NewtonRaphson.m
+% M-file creating the function that implements 
+% Gaussian Elimination for the solution of systems of equations.
+% Written by Hugh Fitzpatrick, S.N. 22341351 for the completion of MS1. 
+
+function x = GaussianElimination(A, b)
+
+    %% Initial definitions
+    Ra = size(A); %% Find size of matrix A
+    n = Ra(1); % Number of rows (and columns) of A, assuming A is square
+    Id = eye(n); % n x n identity matrix
+    Q_tot = Id; % Accumulates the permutation matrix during iteration
+ 
+    % Copy matrices for pivoting, so that originals can be used for final
+    % solution
+    A1 = A; 
+    b1 = b;
+ 
+    for m = 1:n-1 % Loop processes each column in the matrix
+        % Index of current row
+        row_index = [m:n]; % Considers rows from m to n 
+        % Yc contains max values, Ir contains indices of the values 
+        [Yc, Ir] = max(abs(A1(row_index, row_index)));
+        [~, Ic1] = max(Yc);
+ 
+        %% Resolving ties
+        % Find all occurences of the max value
+        tie_indices = find(Yc == max(Yc));
+        Ic1 = tie_indices(1);  % Select the first occurence of the max value
+ 
+        % Store row and col number of the pivot point
+        row_num = Ir(Ic1); 
+        col_num = Ic1;
+ 
+        %% Swap matrix so pivot point is primary element
+        % Swap mth row with row containing the pivot 
+        p = [1:n];
+        p(m) = m-1+row_num;
+        p(m-1+row_num) = m;
+ 
+        % Swap mth col with col containing the pivot
+        q = [1:n];
+        q(m) = m-1+col_num;
+        q(m-1+col_num) = m;
+ 
+        % Create permutation matrices corresponding to row and col swaps
+        P = Id(p, :);
+        Q = Id(:, q);
+ 
+        A1 = P*A1*Q;
+        b1 = P*b1;
+        Q_tot = Q_tot*Q;
+ 
+        %% Eliminiation
+ 
+        % Build vector that is used to eliminate elements below pivot
+        % All values in I_vec are 0 except for the 1st entry in col m
+        I_vec = zeros(1,n);
+        I_vec(1,m) = 1;
+ 
+        % Eliminate elements under pivot point
+        if A1(m,m) ~= 0
+         L1 = Id - ([zeros(m,1); A1(m+1:n,m)]*I_vec)/A1(m,m);
+        else
+            error("ERROR Pivot is equal to 0")
+        end 
+ 
+        % Perform elimination 
+        A1 = L1*A1;
+        b1 = L1*b1;
+    end
+ 
+    %% Back substitution 
+ 
+    % Solution vector 
+    x = zeros(n,1);
+ 
+    % Iterates in reverse, from last row to first
+    for m = 0:n-1
+        if A1(n-m,n-m) ~= 0
+             x(n-m,1) = (b1(n-m,1) - (A1(n-m,:)*x))/A1(n-m,n-m);
+        else 
+            error("ERR: Diagonal element is equal to 0. A1 is not an upper triangular matrix.")
+        end
+        
+    end
+ 
+    % Reverse column swaps
+     x = Q_tot*x;
+ end 
+ 
--- matlab/MP1.m
+++ matlab/MP1.m
@@ -1,24 +1,62 @@
 %% MP1.m
 % Main file implementing the user-defined functions for the solution of
 % the presented complementary symmetry, push-pull power amplifier.
-% Written by Hugh Fitzpatrick, S.N. 22341351 for the completion of Minor 
-% Project 1. 
+% Written by Hugh Fitzpatrick, S.N. 22341351 for the completion of Minor
+% Project 1.
 
 %% Diode Curve Fitting
 
-% Initial Guesses
+clear;
+clc;
 
-n_init = 1;
-R_init = 8.522425;
+V_t = 26e-3;
+Is = 15.2e-12;
 
 % Data Point Selection
 
-V_data = [10e-3 50e-3 200e-3 350e-3 420e-3 480e-3 520e-3 580e-3 620e-3 680e-3 700e-3 740e-3 ...
+V_data = [50e-3 100e-3 200e-3 350e-3 420e-3 480e-3 520e-3 580e-3 620e-3 680e-3 700e-3 740e-3 ...
     760e-3 780e-3 800e-3 840e-3 880e-3 920e-3 940e-3 980e-3];
 
-I_data = [3.73e-12 30e-12 1.323e-9 40.4e-9 197e-9 784e-9 1.94e-6 7.56e-6 18.88e-6 73.6e-6 ...
-     116e-6 284e-6 440e-6 672e-6 1.024e-3 2.240e-3 4.480e-3 8.053e-3 10.29e-3 15.52e-3];
+I_data = [30e-12 123e-12 1.323e-9 40.4e-9 197e-9 784e-9 1.94e-6 7.56e-6 18.88e-6 73.6e-6 ...
+    116e-6 284e-6 440e-6 672e-6 1.024e-3 2.240e-3 4.480e-3 8.053e-3 10.29e-3 15.52e-3];
 
-J = @(x) [Vt, Id];
-V = @(Id, n, R) ln(Id/Is+1) + n*Vt + R*Id;
+N = length(V_data);
 
+alpha = zeros(1, N);
+
+for i = 1:N
+    alpha(i) = V_t*log(I_data(i)/Is - 1);
+end
+
+beta = I_data;
+
+A = [sum(alpha.^2) sum(alpha.*beta);
+     sum(alpha.*beta) sum(beta.^2)];
+
+b = [-sum(V_data.*(-alpha));
+-sum(V_data.*(-beta))];
+
+x = GaussianElimination(A,b);
+
+n = x(1);
+R = x(2);
+
+MSE = (1/N) * sum((V_data - (alpha .* n + beta .* R)).^2);
+
+fprintf("n = %.5f\n", n)
+fprintf("R = %.5f\n", R)
+fprintf("MSE = %.10f", MSE)
+
+V = alpha*n + beta*R;
+
+figure
+plot(V_data, I_data, '-o', 'LineWidth', 2, 'MarkerSize', 8, 'MarkerEdgeColor', 'r')
+xlabel('Voltage (V)')
+ylabel('Current (A)')
+title('I-V Characteristic for IN4148 Diode, with data points highlighted')
+grid on
+
+figure
+plot(V, I_data, 'rx')
+hold on;
+plot(V_data, I_data, 'b-')
